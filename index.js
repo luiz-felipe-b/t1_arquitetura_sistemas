32765a6a89f3122d82096ba7b60552271b234db8
luiz-felipe-b <lfbalaminute@hotmail.com>
2024-09-11 00:22:09 -0300

    feat(t1): obtem previsao a partir de coords

--- index.js
+++ index.js
@@ -20,12 +20,37 @@ async function getCityCoordinates(cityName) {
     }
 }
 
+async function getCurrentWeather(latitude, longitude) {
+    const apiKey = process.env.API_KEY;
+
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`);
+        const data = await response.json();
+
+        if (response.ok) {
+            const { main, weather } = data;
+            const { feels_like } = main;
+            const { description } = weather[0];
+            return { feelsLike: feels_like, description };
+        } else {
+            throw new Error(data.message);
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+        return null;
+    }
+}
+
 async function main() {
     const cityName = process.env.CITY_NAME;
     const coordinates = await getCityCoordinates(cityName);
     console.log(`Coordenadas de ${cityName}:`);
-    console.log(`Latitude: ${coordinates.latitude}`);
-    console.log(`Longitude: ${coordinates.longitude}`);
+    console.log(`+ Latitude: ${coordinates.latitude}`);
+    console.log(`+ Longitude: ${coordinates.longitude}`);
+    const condition = await getCurrentWeather(coordinates.latitude, coordinates.longitude);
+    console.log(`Clima em ${cityName}:`);
+    console.log(`+ Sensação térmica: ${condition.feelsLike} K`);
+    console.log(`+ Descrição: ${condition.description}`);
 }
 
 main()
